8e7d7ffa3dc4f8649f608783c3df15cab05f976e
DiRaiks <t.fane@mail.ru>
2018-05-03 18:47:13 +0300

    update webpack config, need fix problem with babel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ReactDom from 'react-dom';
 
 class App extends Component {
     constructor() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+})
+
 module.export = {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
     module: {
         rules: [
             {
-                test: /\.jsx?&/,
+                test: /.jsx?$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
+                loader: ['babel-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        htmlPlugin
+    ]
 };
