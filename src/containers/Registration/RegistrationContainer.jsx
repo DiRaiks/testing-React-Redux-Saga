d94e4762d2806e2021da42dfb234ac7e195621c1
DiRaiks <t.fane@mail.ru>
2018-06-03 00:25:36 +0300

    added react modal, added view error in modal window if registration is failed

--- src/containers/Registration/RegistrationContainer.jsx
+++ src/containers/Registration/RegistrationContainer.jsx
@@ -3,12 +3,25 @@ import {connect} from 'react-redux';
 import CustomInput from '../../components/CustomInput';
 import style from './registration.scss';
 import * as actionTypes from '../../redux/constants/actionTypes';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
 
 class Registration extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            showModal: false
+        };
     }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.error) {
+            return {showModal: true}
+        } else {
+            return {...prevState}
+        }
+    };
 
     inputChangeHandler = (fieldName, e) => {
         this.setState({
@@ -26,6 +39,14 @@ class Registration extends Component {
             type: actionTypes.REGISTRATION_USER,
             newUser
         })
+    };
+
+    handleOpenModal = () => {
+        this.setState({showModal: true})
+    };
+
+    handleCloseModal = () => {
+        this.setState({showModal: false})
     };
 
     render() {
@@ -48,6 +69,15 @@ class Registration extends Component {
                     )
                 })}
                 <input type='submit' onClick={this.registrationHandler} value='Done'/>
+                <ReactModal
+                    isOpen={this.state.showModal}
+                    contentLabel="onRequestClose Example"
+                    // onRequestClose={this.handleCloseModal}
+                    >
+                    <p>Modal text!</p>
+                    {this.props.error}
+                    <button onClick={this.handleCloseModal}>Close Modal</button>
+                </ReactModal>
             </div>
         )
     }
